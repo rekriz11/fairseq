47f458e55946c417157d82ae595228d86729cdba
rekriz11 <rekriz@seas.upenn.edu>
2022-02-23 16:30:50 -0500

    Update translation_from_pretrained_bart.py

--- fairseq/tasks/translation_from_pretrained_bart.py
+++ fairseq/tasks/translation_from_pretrained_bart.py
@@ -4,7 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import torch
-from fairseq import utils
+from fairseq import utils, search
 from fairseq.data import LanguagePairDataset
 
 from . import register_task
