5d7da4411f523cac7965a615dbb273fdcfbf372d
rekriz11 <rekriz@seas.upenn.edu>
2022-03-09 15:53:51 -0500

    Update sequence_generator.py

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -279,7 +279,7 @@ class SequenceGenerator(nn.Module):
                 ## track previously generated candidates
                 prev_cands = cur_tokens[minor_delim_index:major_delim_index]
                 prev_cands.reverse()
-                prev_restricted_cands[beam_idx] = self.split_list(prev_cands, slot_delimiters[0][0].item())
+                prev_restricted_cands[beam_idx] = self.split_list(prev_cands, slot_delimiters[0][1].item())
 
         print("\n\nrestrict_cands: {}\ncur_restricted_cands: {}\nprev_restricted_cands: {}\ninitial valid_candidates: {}\nforced_cands: {}\ngenerated_forced_cands: {}\ninitial forced_candidates: {}\nslot_delimiters: {}\n".format(restrict_cands, \
             cur_restricted_cands, prev_restricted_cands, valid_candidates, forced_cands, generated_forced_cands, forced_candidates, slot_delimiters))
